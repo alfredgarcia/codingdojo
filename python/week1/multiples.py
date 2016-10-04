b9a6e121b0796406a7123340cf61cc34a086e8f5
alfredgarcia <alfredg2007@yahoo.com>
2016-10-03 18:17:42 -0700

    un comment code

--- python/week1/multiples.py
+++ python/week1/multiples.py
@@ -1,11 +1,11 @@
 ## Part I
-# for count in range(0,1001):
-#     if count % 2 != 0:
-#         print count
-#         count += 1
+for count in range(0,1001):
+    if count % 2 != 0:
+        print count
+        count += 1
 
 ## Part II
-# for count in range(5,1000005):
-#     if count % 5 == 0:
-#         print count
-#         count +=1
+for count in range(5,1000005):
+    if count % 5 == 0:
+        print count
+        count += 1
