18976d4184a816b581ae40861a5e63076727e853
alfredgarcia <alfredg2007@yahoo.com>
2016-10-03 18:15:46 -0700

    day 1 set of exercises

---
+++ python/week1/python_intro.py
@@ -0,0 +1,38 @@
+# print statements
+print "hello world"
+import this
+# variables
+cat = "kitty"
+print cat
+
+# functions
+def thisIsAFunction():
+    print "hello"
+
+thisIsAFunction()
+
+# if statements
+if 2 > 3:
+    print "what?"
+else:
+    print "huh?"
+
+# for loops
+for i in range(1,255 + 1):
+    print i
+
+# oneTo2555
+def oneTo255():
+    for i in range(1, 256):
+        print i
+
+names = ['KB', 'Oliver', 'Mikey', 'John', 'Michael']
+print '\n'.join(names)
+
+for count in range(0,5):
+    print "looping - ", count
+
+count = 0
+while count < 5:
+    print 'looping - ', count
+    count += 1
